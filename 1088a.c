f7cedb32dba41a645217ed9aa8a5f8adc8a8f507
jordandesouza <jordan.souza@unifesp.br>
2025-10-28 22:08:54 -0300

    Refactor to count inversions using merge sort

--- 1088a.c
+++ 1088a.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+typedef long long ll;
 
-    int numero, posicao, menor;
-    scanf("%d", &numero);
+static ll merge_count(int *a, int *tmp, int l, int r) {
+    if (r - l <= 1) return 0;
+    int m = (l + r) / 2;
+    ll inv = merge_count(a, tmp, l, m) + merge_count(a, tmp, m, r);
 
-    //tamanho do array definido pelo usuário
-    int lista[numero];
-
-    //valores do usuário sendo alocado na array
-    for(int i = 0; i< numero; i++){
-        scanf("%d", &lista[i]);
-
+    int i = l, j = m, k = l;
+    while (i < m && j < r) {
+        if (a[i] <= a[j]) tmp[k++] = a[i++];
+        else { tmp[k++] = a[j++]; inv += (ll)(m - i); }
     }
+    while (i < m) tmp[k++] = a[i++];
+    while (j < r) tmp[k++] = a[j++];
+    for (i = l; i < r; i++) a[i] = tmp[i];
 
-    menor = lista[0];
+    return inv;
+}
 
-    //encontrando o menor valor e sua posição
-    for(int i=0; i< numero; i++){
-        if(lista[i] < menor){
-            posicao = i;
-            menor = lista[i];
-        }
+int main(void) {
+    int N;
+    while (scanf("%d", &N) == 1) {
+        if (N == 0) break;
+
+        int *a = (int*)malloc(N * sizeof(int));
+        int *tmp = (int*)malloc(N * sizeof(int));
+        for (int i = 0; i < N; i++) scanf("%d", &a[i]);
+
+        ll inv = merge_count(a, tmp, 0, N);
+        puts((inv % 2) ? "Marcelo" : "Carlos");
+
+        free(a);
+        free(tmp);
     }
-
-
-    printf("Menor valor: %d\nPosicao: %d\n", menor, posicao);
+    return 0;
 }
