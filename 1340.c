017a4b45417116dba432c0b8a5d83272035df32a
jordandesouza <jordan.souza@unifesp.br>
2025-10-29 01:07:34 -0300

    Update 1340.c

--- 1340.c
+++ 1340.c
@@ -1,84 +1,54 @@
 #include <stdio.h>
 
-#define MAXN 100000
-
-typedef struct {
-    int a[1005];
-    int top;
-} Stack;
-
-typedef struct {
-    int a[1005];
-    int l, r;
-} Queue;
-
-typedef struct {
-    int a[1005];
-    int sz;
-} MaxHeap;
-
-void st_init(Stack *s){ s->top = 0; }
-void st_push(Stack *s, int x){ s->a[s->top++] = x; }
-int  st_pop(Stack *s){ return s->a[--s->top]; }
-int  st_empty(Stack *s){ return s->top == 0; }
-
-void q_init(Queue *q){ q->l = q->r = 0; }
-void q_push(Queue *q, int x){ q->a[q->r++] = x; }
-int  q_pop(Queue *q){ return q->a[q->l++]; }
-int  q_empty(Queue *q){ return q->l == q->r; }
-
-void hp_init(MaxHeap *h){ h->sz = 0; }
-void hp_push(MaxHeap *h, int x){
-    int i = ++h->sz;
-    while (i > 1 && h->a[i/2] < x){ h->a[i] = h->a[i/2]; i /= 2; }
-    h->a[i] = x;
-}
-int hp_pop(MaxHeap *h){
-    int ret = h->a[1], x = h->a[h->sz--], i = 1, j;
-    while (i*2 <= h->sz){
-        j = i*2;
-        if (j+1 <= h->sz && h->a[j+1] > h->a[j]) j++;
-        if (h->a[j] <= x) break;
-        h->a[i] = h->a[j];
-        i = j;
-    }
-    h->a[i] = x;
-    return ret;
-}
-int hp_empty(MaxHeap *h){ return h->sz == 0; }
-
 int main(void){
     int n;
     while (scanf("%d", &n) == 1){
-        Stack st; Queue q; MaxHeap h;
-        int isStack = 1, isQueue = 1, isPq = 1;
-        st_init(&st); q_init(&q); hp_init(&h);
+        int stk[1000], stksz = 0;
+        int que[1000], qh = 0, qt = 0;
+        int pq[1000], pqs = 0;
+
+        int ok_stack = 1, ok_queue = 1, ok_pq = 1;
 
         for (int i = 0; i < n; i++){
-            int t, x;
-            scanf("%d %d", &t, &x);
-            if (t == 1){
-                if (isStack) st_push(&st, x);
-                if (isQueue) q_push(&q, x);
-                if (isPq)    hp_push(&h, x);
+            int op, x;
+            scanf("%d %d", &op, &x);
+
+            if (op == 1){
+                if (ok_stack) stk[stksz++] = x;
+                if (ok_queue) que[qt++] = x;
+                if (ok_pq)    pq[pqs++]  = x;
             } else {
-                if (isStack){
-                    if (st_empty(&st) || st_pop(&st) != x) isStack = 0;
+                if (ok_stack){
+                    if (stksz == 0) ok_stack = 0;
+                    else if (stk[stksz-1] != x) ok_stack = 0;
+                    else stksz--;
                 }
-                if (isQueue){
-                    if (q_empty(&q) || q_pop(&q) != x) isQueue = 0;
+                if (ok_queue){
+                    if (qh == qt) ok_queue = 0;
+                    else if (que[qh] != x) ok_queue = 0;
+                    else qh++;
                 }
-                if (isPq){
-                    if (hp_empty(&h) || hp_pop(&h) != x) isPq = 0;
+                if (ok_pq){
+                    if (pqs == 0) ok_pq = 0;
+                    else{
+                        int idx = 0;
+                        for (int j = 1; j < pqs; j++)
+                            if (pq[j] > pq[idx]) idx = j;
+                        if (pq[idx] != x) ok_pq = 0;
+                        else{
+                            pq[idx] = pq[pqs-1];
+                            pqs--;
+                        }
+                    }
                 }
             }
         }
 
-        int cnt = isStack + isQueue + isPq;
+        int cnt = ok_stack + ok_queue + ok_pq;
         if (cnt == 0) puts("impossible");
         else if (cnt > 1) puts("not sure");
-        else if (isStack) puts("stack");
-        else if (isQueue) puts("queue");
+        else if (ok_stack) puts("stack");
+        else if (ok_queue) puts("queue");
         else puts("priority queue");
     }
     return 0;
